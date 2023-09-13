4236d07e3dcdd5f1b28128dc441437638295783b
w-cypriano <weldev16@gmail.com>
2023-09-13 06:22:34 -0300

    criando o bucket s3

--- bucket_s3.tf
+++ bucket_s3.tf
@@ -1,4 +1,10 @@
-
 resource "aws_s3_bucket" "mybucket" {
-  bucket = "${}"
+  bucket = "Mybucket"
+
+  tags = {
+		Service	    = "curso terraform"
+		ManagedBy   = "terraform"
+		Environment = var.environment
+		Owner       = "Wellington Cypriano"
+  }
 }
